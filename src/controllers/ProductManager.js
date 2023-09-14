f941b4f8658ee6198f77dd1a03956852d301e94e
CristiSalazar <cristina.salazar125@gmail.com>
2023-09-14 20:22:52 -0300

    nuevos cambios 2

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -1,4 +1,5 @@
 import {promises as fs} from "fs"
+import {nanoid} from "nanoid"
 
 class ProductManager {
     constructor() {
@@ -11,13 +12,52 @@ class ProductManager {
     }
 
     writeProducts = async (product) => {
+        await fs.writeFile(this.path, JSON.stringify(product))
+    }
+
+    exist = async (id) => {
+        let products = await this.readProducts()
+        return products.find(prod => prod.id === id)
+    }
+
+    addProducts = async (product) => {
         let productsOld = await this.readProducts()
+        product.id = nanoid()
         let todosLosProductos = [...productsOld, product]
-        await fs.writeFile(this.path, JSON.stringify(todosLosProductos))
+        await this.writeProducts(todosLosProductos)
         return "Producto Agregado"
     }
 
-    getproducts = async () => {
+    getProducts = async () => {
+        return await this.readProducts()
+    }
+
+    getProductsById = async (id) => {
+        let productsById = await this.exist(id)
+        if (!productsById) return "Producto no existe"
+        return productsById
+    }
+
+
+    updateProducts = async(id, product) => {
+        let productsById = await this.exist(id)
+        if (!productsById) return "Producto no existe"
+        await this.deleteProducts(id)
+        let productsOld = await this.readProducts()
+        let products = [{...product, id : id}, ... productsOld]
+        await this.writeProducts(products)
+        return "El producto fue actualizado"
+    }
+
+    deleteProducts = async (id) => {
+        let products = await this.readProducts()
+        let existingProducts = products.some(prod => prod.id === id) 
+        if (existingProducts) {
+            let filterProducts = products.filter(prod => prod.id != id) 
+            await this.writeProducts(filterProducts)
+            return "Producto eliminado"
+        }
+        return "Producto que desea eliminar no existe"
 
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,36 @@ import ProductManager from "./controllers/ProductManager.js"
 const product = new ProductManager()
 
 const app = express()
-const PORT = 4000
+const PORT = 8080
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get("/products", async (req, res) => {
     res.send(await product.getProducts())
+})
 
+app.get("/products/:id", async (req, res) => {
+    let id = req.params.id
+    res.send(await product.getProductsById(id))
 })
 
 app.post("/products", async (req, res) => {
     let newProduct = req.body
-    res.send(await product.writeProducts(newProduct))
+    res.send(await product.addProducts(newProduct))
 
 })
 
+app.put("/products/:id", async (req, res) => {
+    let id = req.params.id
+    let updateProduct = req.body
+    res.send(await product.updateProducts(id, updateProduct))
+})
+
+app.delete("/products", async (req, res) => {
+    let id = req.params.id
+    res.send(await product.deleteProducts(id))
+}) 
+
 app.listen(PORT, () => {
-    console.log("Servidor express puerto 4000")
+    console.log("Servidor express puerto 8080")
 })
